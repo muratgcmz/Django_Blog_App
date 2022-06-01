0b91c0fcacb407bba41a94acf68687aca6acd2e1
muratgcmz <gocmezmurat@gmail.com>
2022-06-02 00:28:47 +0300

    crud done

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,12 @@
-from .views import post_list, post_create, post_update
+from .views import post_list, post_create, post_update, post_delete, post_details
 from django.urls import path
 
 urlpatterns = [
     path('', post_list, name='home'),
     path('create/', post_create, name='create'),
     path('update/<int:id>', post_update, name='update'),
+    path('delete/<int:id>', post_delete, name='delete'),
+    path('details/<int:id>', post_details, name='details'),
+    
     
 ]
--- blog/views.py
+++ blog/views.py
@@ -36,4 +36,24 @@ def post_update(request, id):
     
     return render(request, 'blog/post_update.html', context)
 
+
+def post_delete(request, id):
+    post = Post.objects.get(id=id)
+    if request.POST :
+        post.delete()
+        return redirect('home')
+    context = {
+        'post': post
+    }
+    
+    return render(request, 'blog/post_delete.html', context)
+
+def post_details(request, id):
+    post = Post.objects.get(id=id)
+    
+    context = {
+        'post': post
+    }
+    
+    return render(request, 'blog/post_details.html', context)
     
