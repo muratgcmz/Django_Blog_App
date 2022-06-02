a2cf441665ad4c52835260ad984ff82c6c2d17a1
muratgcmz <gocmezmurat@gmail.com>
2022-06-02 23:59:06 +0300

    login done

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import user_logout
+from .views import user_logout, user_login
 
 urlpatterns = [
     path('logout/', user_logout, name='logout'),
+    path('login/', user_login, name='user_login'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,28 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import logout
+from django.contrib.auth import logout, login
+from django.contrib.auth.forms import AuthenticationForm
 # Create your views here.
 
 
 def user_logout(request):
     messages.success(request, 'You logged out!')
     logout(request)
-    return redirect('home')
+    return render(request, 'user/logout.html')
     
+def user_login(request):
+    form = AuthenticationForm(request, data=request.POST)
+    
+    if form.is_valid():
+        # username = form.cleaned_data.get('username')
+        # password = form.cleaned_data.get('password')
+        # user = authenticate(username=username, password=password)
+
+        user = form.get_user()
+
+        if user :
+            messages.success(request, 'login successfull')
+            login(request, user)
+        return redirect('home')
+
+    return render(request, 'user/user_login.html',{"form": form})
